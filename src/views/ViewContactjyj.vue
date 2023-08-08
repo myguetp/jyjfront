c07d5d722c3599f0f8c4649a285bb4c2a63e9f8a
miguel <jmtiradop19@gmail.com>
2023-08-07 19:48:45 -0500

    fix(layout): create layout by menu

--- src/views/ViewContactjyj.vue
+++ src/views/ViewContactjyj.vue
@@ -1,14 +1,16 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
+import MainLayout from '../layouts/MainLayout.vue'
 
 export default defineComponent({
+  components: {MainLayout},
   setup() {
     return {}
   },
 })
 </script>
 <template>
-  <div>
-    hola contacto
-  </div>
+  <MainLayout>
+    <p>hola contacto</p>
+  </MainLayout>
 </template>
