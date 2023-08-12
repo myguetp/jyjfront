b9127b6792e9c90b4ac13fabe98abef83738148b
miguel <jmtiradop19@gmail.com>
2023-08-11 22:55:09 -0500

    feat(startjyj): image and social media

--- src/components/MenuNavBatjyj.vue
+++ src/components/MenuNavBatjyj.vue
@@ -11,20 +11,24 @@ export default defineComponent({
 </script>
 
 <template>
-  <section class="flex items-center justify-center text-center mt-4">  
+  <section class="flex items-center justify-center text-center mt-0">  
   <header class="flex gap-8">
     <RouterLink to="/">
-      <div>imagen</div>
+      <div>
+        <img src="@/assets/img/empresa.jpeg" alt="" class="w-[80px] h-[80px] rounded-l-[200px]">
+      </div>
     </RouterLink>
-    <nav class="flex gap-8 items-cemter text-center justify-center mt-2">
+    <nav class="flex gap-8 items-cemter text-center justify-center mt-6">
       <RouterLink to="/Advertisements">Anuncios</RouterLink>
       <RouterLink to="/Us">Nosotros</RouterLink>
       <RouterLink to="/Contact">Contacto</RouterLink>
       <RouterLink to="/Leases">Arriendos</RouterLink>
       <RouterLink to="/Sales">Ventas</RouterLink>
     </nav>
-    <div class="flex gap-4">
-    <TheButtonjyjVue texto="Ingresar" />
+    <div class="mt-6">
+    <p>Ingresar</p>
+    </div>
+    <div class="flex gap-4 mt-2">
     <TheButtonjyjVue texto="Publica gratis" />
     </div>
   </header>
---
+++ src/views/components/inicio/Staryjyj.vue
@@ -0,0 +1,51 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+import TheButtonjyj from '@/components/TheButtonjyj.vue'
+import IconFacebook from '@/components/icons/IconFacebook.vue'
+import IconInstagram from '@/components/icons/IconInstagram.vue'
+import IconPinterest from '@/components/icons/IconPinterest.vue'
+import IconYoutube from '@/components/icons/IconYouTube.vue'
+import IconLinkedin from '@/components/icons/IconLinkedin.vue'
+
+export default defineComponent({
+  components: { TheButtonjyj, IconFacebook, IconInstagram, IconPinterest, IconYoutube, IconLinkedin },
+  setup() {
+    return {}
+  },
+})
+</script>
+
+<template>
+  <section class="block">
+    <div class="flex  items-center gap-10 ">
+      <div class="flex items-center w-[850px] p-10">
+        <div class="block items-center ml-16">
+          <h2 class="text-[40px] font-bold">Creamos produtos </h2>
+          <h2 class="text-[40px] font-bold">digitales para tu conjunto </h2>
+          <h2 class="text-[40px] font-bold">y/o edificioresidencial</h2>
+          <p>Diseñamos y desarrollamos las apps siempre pensando en los propietarios</p>
+          <div class="mt-4 font-bold">
+          <TheButtonjyj texto="Trabajemos juntos" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <img src="@/assets/img/firstImage.jpg" alt="" class="w-[900px] h-[500px] rounded-l-[200px]">
+      </div>
+    </div>
+  <footer class="mt-10">
+    <div class="flex justify-center items-center ">
+    <p>Encuentranos en</p>
+    </div>
+    <div class="flex justify-center items-center mb-10 mt-2">
+      <section class="flex gap-20">
+        <a class="cursor-pointer" href="#"> <IconFacebook /> </a>
+        <a class="cursor-pointer" href="#">  <IconInstagram /> </a>
+        <a class="cursor-pointer" href="#"> <IconPinterest /> </a>
+        <a class="cursor-pointer" href="#"> <IconYoutube /> </a>
+        <a class="cursor-pointer" href="#"> <IconLinkedin /> </a>
+      </section>
+    </div>
+  </footer>
+  </section>
+</template>
