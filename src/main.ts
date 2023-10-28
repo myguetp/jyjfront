8f73bcc4b8746f4e0799f55b4a751820409f16b4
miguel <jmtiradop19@gmail.com>
2023-10-28 18:23:51 -0500

    28102023 feat(main): library crousel

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import  { VueQueryPlugin } from 'vue-query'
+import { VueCarousel } from 'vue-carousel'
 
 
 import App from './App.vue'
@@ -13,5 +14,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(VueQueryPlugin)
+app.use(VueCarousel)
 
 app.mount('#app')
