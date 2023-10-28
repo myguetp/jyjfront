739815c215233a9fcca65bd4766b1b7bd328c674
miguel <jmtiradop19@gmail.com>
2023-10-28 18:20:55 -0500

    28102023 feat(components): components service leases and sales

---
+++ src/components/icons/IconApartment.vue
@@ -0,0 +1,12 @@
+<template>
+  <svg width="20" height="20" viewBox="0 0 49 48" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+<rect x="0.5" width="48" height="48" fill="url(#pattern0)"/>
+<defs>
+<pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
+<use xlink:href="#image0_129316_4" transform="scale(0.0208333)"/>
+</pattern>
+<image id="image0_129316_4" width="48" height="48" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAACXBIWXMAAAsTAAALEwEAmpwYAAAA9klEQVRoge2Zyw2DMBBEpwCOFDClUQRlUASlbQH0kCBxsRQ8OOLrZJ40t2XhHaxdGaCcYc54UYYvvquYmPO6KGGBDYF+TndwelwowBP6ExbQBCwgISygCfy4QCRRPXI1xM0C6SDKoWoIC2gCWoBJcqgawodYE7CAhLCAJmABCWEBTUALjElyqBriZoHqJ3H1Al2SHKqG8CHWBCwgISyw0GD95mxCJQJpo1y45wUF742jGlUp0GL7/r5dea7Dw+fAFo+fxFtULzCi8m10L4QFNAELSAgLaAIWkBAWWChZp8/+0T1lapoSASaNnhb+hUDJOn1XPtb4N84o9+CAUTK0AAAAAElFTkSuQmCC"/>
+</defs>
+</svg>
+</template>
+    
