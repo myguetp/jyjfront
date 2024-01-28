1af9a385bba64d301358c59f9dee158fda27e102
miguel <jmtiradop19@gmail.com>
2024-01-27 20:17:31 -0500

    all

--- src/views/ViewLeasesjyj.vue
+++ src/views/ViewLeasesjyj.vue
@@ -19,10 +19,6 @@ export default defineComponent({
         alt: "Picture 3",
       },
       {
-        src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
-        alt: "Picture 4",
-      },
-      {
         src: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
         alt: "Picture 5",
       },
